24b57e5551f30e45f75c2ebb077d5ed549c6c304
Stanislav Fedorniak <stanislav.fedorniak@gmail.com>
2026-09-01 11:11:31 +0300

    feat(core): add ICategoryService interface and category DTOs

---
+++ TodoApp.Core/DTOs/CategoryDtos.cs
@@ -0,0 +1,5 @@
+﻿namespace TodoApp.Core.DTOs;
+
+public record CategoryDto(Guid Id, string Name);
+public record CategoryCreateDto(string Name);
+public record CategoryUpdateDto(string Name);
--- TodoApp.Core/Interfaces/ICategoryService.cs
+++ TodoApp.Core/Interfaces/ICategoryService.cs
@@ -2,9 +2,9 @@
 
 public interface ICategoryService
 {
-    public async Task<IEnumerable<CategoryService>> GetAllAsync();
-    public async Task<CategoryService> GetByIdAsync(Guid id);
-    public async Task<CategoryService> CreateAsync(RequestCreateBody request);
-    public async Task UpdateAsync(RequestUpdateBody request);
-    public async Task DeleteAsync(Guid id);
+    Task<IEnumerable<CategoryDTO>> GetAllAsync();
+    Task<CategoryDTO> GetByIdAsync(Guid id);
+    Task<CategoryDTO> CreateAsync(CategoryCreateDto request);
+    Task UpdateAsync(Guid id, CategoryUpdateDto request);
+    Task DeleteAsync(Guid id);
 }
